e74c22fbac5ff998672527eeb309550973551c82
임성애 <tjddo2791@naver.com>
2021-06-29 13:32:25 +0900

    Add deploy workflow

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to 써엉 Vue.js App" />
   </div>
 </template>
 
